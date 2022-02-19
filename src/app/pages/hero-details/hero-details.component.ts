05ef092074838623a22b31e7bf74f514d0d5bf6f
Diego Alves Garcia <diego.alvesgarcia@gmail.com>
2022-02-18 23:34:28 -0300

    refact: configuração de modelo de arquivos de CSS

--- src/app/pages/hero-details/hero-details.component.ts
+++ src/app/pages/hero-details/hero-details.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hero-details',
   templateUrl: './hero-details.component.html',
-  styleUrls: ['./hero-details.component.sass']
+  styleUrls: ['./hero-details.component.scss']
 })
 export class HeroDetailsComponent implements OnInit {
-
+  caracterName: string;
+  description: string;
   constructor() { }
 
   ngOnInit(): void {
+    this.caracterName = 'Wolverine';
+    this.description = "Born with super-human senses and the power to heal from almost any wound, Wolverine was captured by a secret Canadian organization and given an unbreakable skeleton and claws. Treated like an animal, it took years for him to control himself. Now, he's a premiere member of both the X-Men and the Avengers."
   }
 
 }
--- src/app/pages/heroes-list/heroes-list.component.ts
+++ src/app/pages/heroes-list/heroes-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './heroes-list.component.html',
-  styleUrls: ['./heroes-list.component.sass']
+  styleUrls: ['./heroes-list.component.scss']
 })
 export class HeroesListComponent implements OnInit {
 
