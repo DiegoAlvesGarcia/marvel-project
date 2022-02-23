5570cc64e2bb514fd18b8f860a5b46135c1b43b1
Diego Alves Garcia <diego.alvesgarcia@gmail.com>
2022-02-22 22:49:56 -0300

    feat: teste unitário do app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,47 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { routerStub } from './shared/stubs/router.stub';
+import { Location } from '@angular/common';
+import { locationStub } from './shared/stubs/location.stub';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let app: AppComponent;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
+      ],
+      providers: [
+        { provide: Router, useValue: routerStub },
+        { provide: Location, useValue: locationStub }
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
   })
   
   it('should create the app', () => {
     expect(app).toBeTruthy();
   });
+
+  it('should to test the function previousRoute', () => {
+    app.previousRoute()
+    expect(locationStub.back).toHaveBeenCalled();
+  })
+
+  it('should to test the function nextRoute', () => {
+    app.nextRoute()
+    expect(locationStub.forward).toHaveBeenCalled();
+  })
 });
